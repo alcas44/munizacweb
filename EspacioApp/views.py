a4834b3111d495cad969c6cd93849545002ad1d1
alcas44 <cescmazariegos@gmail.com>
2022-02-02 16:22:55 -0600

    Gracias a Dios Cambios en Dash Usuario Martes MCMA

--- EspacioApp/views.py
+++ EspacioApp/views.py
@@ -5,3 +5,7 @@ def principal(request):
         return redirect('/')
     else:   
         return render(request,'EspacioApp/principal.html')
+
+
+#ver si ademas de preguntar si esta autenticado preguntar si tambien esta activado el usuario
+#y mandar mensaje a la raiz y decir que no esta autenticado y activo
